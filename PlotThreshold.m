4d2273da1ca388147d45ebb85db158a1069ae21f
rop <roedorpi@gmail.com>
2024-08-19 15:13:43 +0200

    update after last measurements of IP30 sensitivity

--- PlotThreshold.m
+++ PlotThreshold.m
@@ -30,7 +30,7 @@ Coll = [0.8 0.2 0.2
 for i = 1:length(Thresh)
     t = fio.(Thresh{i});
     tr = t.Raw;
-    Indx = 1:size(tr);
+    Indx = 1:size(tr,1);
     if strcmp(t.Ear,'Left')
         Ear = 1;
         Title = 'Hearign thresholds Ascending method (IP30)  -- Left ear';
@@ -40,17 +40,20 @@ for i = 1:length(Thresh)
     end
     for j = 1:size(tr,3)
         
-            tr_s(:,1,j) = tr(:,1,j)+ldB(j,Ear);
-            tr_s(:,2:3,j) = tr(:,2:3,j);
-            %lv = find(tr_s(:,3,j)== 0,1)-1;
-            %if isempty(lv)
-               pfc(j,Ear) = plot(a(Ear),Indx(tr_s(:,2,j)==1),tr_s(tr_s(:,2,j)==1,1,j),'+','Color',Coll(j,:));
-               plot(a(Ear),Indx(tr_s(:,2,j)==0),tr_s(tr_s(:,2,j)==0,1,j),'o','Color',Coll(j,:))
-            %else
-            %   pfc(j,Ear) = plot(a(Ear),Indx(tr_s(1:lv,2,j)==1),tr_s(tr_s(1:lv,2,j)==1,1,j),'+','Color',Coll(j,:));
-            %   plot(a(Ear),Indx(tr_s(1:lv,2,j)==0),tr_s(tr_s(1:lv,2,j)==0,1,j),'o','Color',Coll(j,:))
-            %end
-        
+            tr_s(:,1:3,j) = [tr(:,1,j)+ldB(j,Ear) tr(:,2:3,j)];
+           
+           % lv = find(tr_s(:,3,j)== 0,1)-1;
+           if sum(tr_s(:,2)) ~= 0
+               pfc(j,Ear) = plot(a(Ear),Indx(tr_s(:,2,j)==1),...
+                   tr_s(tr_s(:,2,j)==1,1,j),'+','Color',Coll(j,:));
+               plot(a(Ear),Indx(tr_s(:,2,j)==0),...
+                   tr_s(tr_s(:,2,j)==0,1,j),'o','Color',Coll(j,:)) 
+           else
+               pfc(j,Ear) = plot(a(Ear),Indx(tr_s(:,2,j)==0),...
+                   tr_s(tr_s(:,2,j)==0,1,j),'o','Color',Coll(j,:));
+               
+           end
+           
         Legs{j} = sprintf('%i Hz',t.Estimate.Freqs(j)); 
     end
     clear tr_s
